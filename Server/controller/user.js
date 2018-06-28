56a5d014513904c71ef371596295a5afebe0c88d
luckzman <you@example.com>
2018-06-28 21:20:47 +0100

    setup login controller with database connection

--- Server/controller/user.js
+++ Server/controller/user.js
@@ -13,11 +13,44 @@ const User = {
                 data: {
                     user: user.rows[0]
                 }
-                // user: user.rows[0]
             });
       
         });
     },
+    loginUser(req, res, next) {
+        const query = 'SELECT email, password FROM users WHERE email = $1';
+        const values = [req.body.email];
+
+        db.query(query, values, (err, user) => {
+            if(!user){
+                return res.status(404).json({
+                    success: false,
+                    message: 'user not available'
+                })
+            }
+            else if(user){
+                let newUser = '';
+                user.rows.map(data => {
+                    if(req.body.password === data.password) {
+                        newUser = data.password;      
+                    }
+                })
+                if(req.body.password !== newUser){
+                    return res.status(400).json({
+                        success: false,
+                        message: 'Incorrect password'
+                    })
+                }
+                return res.status(200).json({
+                    success: true,
+                    message: 'User signin successful'
+                })
+            }
+            else {
+                return next(err)
+            }
+        });
+    }
 };
 
 export default User;
--- Server/route/user.js
+++ Server/route/user.js
@@ -4,5 +4,6 @@ import userController from './../controller/user';
 const router = express.Router();
 
 router.post('/signup', userController.createUser);
+router.post('/login', userController.loginUser);
 
 export default router;
